075a5f26dc8f17891edfd4b884296532e1bdfeb6
Taurus Silver <jrchavez_07@outlook.com>
2022-10-05 20:17:42 -0400

    Initial commit

--- Chapter-05/Ex-04/src/strend.c
+++ Chapter-05/Ex-04/src/strend.c
@@ -6,19 +6,40 @@
  */
 #include <stdio.h>
 
+void test(void);
+
 static int strend(char *s, char *t);
 
 int
 main(void)
 {
-
+	test();
 	return 0;
+}
+
+
+void
+test(void)
+{
+	printf("> %d\n", strend("a", ""));
+	printf("> %d\n", strend("duck", "duck"));
+	printf("> %d\n", strend("Unsubtle", "subtle"));
+	printf("> %d\n", strend("frankly", "frank"));
+	printf("> %d\n", strend("aaaaaX", "aaaaX"));
+	return;
 }
 
 
 static int
 strend(char *s, char *t)
 {
+	char *_s, *_t;
+
+	do {
+		for (_s = s, _t = t; *_s == *_t; _s++, _t++)
+			if (*_s == '\0')
+				return 1;
+	} while (*s++);
 	return 0;
 }
 
